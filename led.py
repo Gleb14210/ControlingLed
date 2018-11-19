9f27c906d440227ea79107e226d42c3ea4f9d4fb
Gleb14210 <zador.gleb@gmail.com>
2018-11-19 20:53:06 +0200

    Test commit

--- led.py
+++ led.py
@@ -1,21 +1,14 @@
-#file = open("testfile.txt", "w")
-
-#file.write( "gpio=%d=%s,%s" % (17, "op", "dh") )
-#file.truncate(0)
-
-#file.close()
-
 import RPi.GPIO as GPIO
-#import os
 import time
-#import subprocess
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
+f = 10
 
 while True:
-	GPIO.output(12, 0)
-	time.sleep(0.3)
-	GPIO.output(12, 1)
-	time.sleep(0.3)
+        GPIO.output(12, 0)
+        time.sleep(1/f)
+        GPIO.output(12, 1)
+        time.sleep(1/f)
 
 GPIO.cleanup()
